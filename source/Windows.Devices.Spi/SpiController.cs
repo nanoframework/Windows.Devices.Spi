ea64b981156eb7a3172624c77399cc282322000e
José Simões <josesimoes@users.noreply.github.com>
2018-11-09 15:27:52 +0000

    Add lazy initialization to SpiController (#28)

--- source/Windows.Devices.Spi/SpiController.cs
+++ source/Windows.Devices.Spi/SpiController.cs
@@ -12,10 +12,46 @@ namespace Windows.Devices.Spi
     /// </summary>
     public sealed class SpiController
     {
-        // we can have only one instance of the SpiController
-        private static SpiController s_instance = new SpiController();
+        // this is used as the lock object 
+        // a lock is required because multiple threads can access the I2C controller
+        readonly static object _syncLock = new object();
 
-        internal static Hashtable s_deviceCollection = new Hashtable();
+        // we can have only one instance of the SpiController
+        // need to do a lazy initialization of this field to make sure it exists when called elsewhere.
+        private static SpiController s_instance;
+
+        // backing field for DeviceCollection
+        private static Hashtable s_deviceCollection;
+
+        /// <summary>
+        /// Device collection associated with this <see cref="SpiController"/>.
+        /// </summary>
+        /// <remarks>
+        /// This collection is for internal use only.
+        /// </remarks>
+        internal static Hashtable DeviceCollection
+        {
+            get
+            {
+                if (s_deviceCollection == null)
+                {
+                    lock (_syncLock)
+                    {
+                        if (s_deviceCollection == null)
+                        {
+                            s_deviceCollection = new Hashtable();
+                        }
+                    }
+                }
+
+                return s_deviceCollection;
+            }
+
+            set
+            {
+                s_deviceCollection = value;
+            }
+        }
 
         /// <summary>
         /// Gets the default SPI controller on the system.
@@ -23,6 +59,17 @@ namespace Windows.Devices.Spi
         /// <returns>The default SPI controller on the system, or null if the system has no SPI controller.</returns>
         public static SpiController GetDefault()
         {
+            if (s_instance == null)
+            {
+                lock (_syncLock)
+                {
+                    if (s_instance == null)
+                    {
+                        s_instance = new SpiController();
+                    }
+                }
+            }
+
             return s_instance;
         }
 
